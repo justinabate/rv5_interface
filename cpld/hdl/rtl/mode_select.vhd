477eff22353705a823c42463f1124a55d3113f3e
justinjabate <justin.j.abate@gmail.com>
2021-07-29 13:42:15 -0400

    hdl cleanup

--- cpld/hdl/rtl/mode_select.vhd
+++ cpld/hdl/rtl/mode_select.vhd
@@ -11,10 +11,10 @@ generic (
 	g_num_dig : natural := 4  -- number of digits on display
 );
 port (
-	clk          : in  STD_LOGIC;
-	mode_pb      : in  STD_LOGIC_VECTOR (5 downto 0); -- active high PB input
-	mode_display : out STD_LOGIC_VECTOR (g_num_seg*g_num_dig-1 downto 0); -- active low LED output	
-	mode_vout    : out STD_LOGIC_VECTOR (g_r2r_size-1 downto 0) -- mode voltage bias
+	i_clk      : in  STD_LOGIC;
+	i_pushbttn : in  STD_LOGIC_VECTOR (5 downto 0); -- active high PB input
+	o_7seg_dat : out STD_LOGIC_VECTOR (g_num_seg*g_num_dig-1 downto 0); -- active low LED output	
+	o_r2r_vout : out STD_LOGIC_VECTOR (g_r2r_size-1 downto 0) -- mode voltage bias
 ); end mode_select;
 
 
@@ -27,74 +27,74 @@ begin
 
 	--! when a pushbutton press is detected (pulled down), 
 	--! change the current state to the respective mode 
-	p_state_update : process (clk) is begin
-		if ((clk'event) and (clk='1')) then
+	p_state_update : process (i_clk) is begin
+		if ((i_clk'event) and (i_clk='1')) then
 			
-			if (mode_pb(0) = '0') then 
+			if (i_pushbttn(0) = '0') then 
 				fsm_cs <= m0;
 			end if;
 			
-			if (mode_pb(1) = '0') then 
+			if (i_pushbttn(1) = '0') then 
 				fsm_cs <= m1;					
 			end if;
 			
-			if (mode_pb(2) = '0') then 
+			if (i_pushbttn(2) = '0') then 
 				fsm_cs <= m2;					
 			end if;
 			
-			if (mode_pb(3) = '0') then 
+			if (i_pushbttn(3) = '0') then 
 				fsm_cs <= m3;					
 			end if;
 			
-			if (mode_pb(4) = '0') then 
+			if (i_pushbttn(4) = '0') then 
 				fsm_cs <= m4;					
 			end if;
 			
-			if (mode_pb(5) = '0') then 
+			if (i_pushbttn(5) = '0') then 
 				fsm_cs <= m5;
 			end if;
 			
 		end if;
 	end process;
 
-	p_state_output : process (clk) is begin
-		if ((clk'event) and (clk='1')) then
+	p_state_output : process (i_clk) is begin
+		if ((i_clk'event) and (i_clk='1')) then
 			case (fsm_cs) is 
 				
 				-- m0: MODX -> 0V0
 				when (m0) => 
-					mode_display <= "0101011" & "0111111" & "1011110" & "0000000"; -- [M] [O] [d] [ ] 
-					mode_vout <= '0' & x"0"; 
+					o_7seg_dat <= "0101011" & "0111111" & "1011110" & "0000000"; -- [M] [O] [d] [ ] 
+					o_r2r_vout <= '0' & x"0"; 
 
 				-- m1: GATE -> 0V3
 				when (m1) => 
-					mode_display <= "0111101" & "1110111" & "1110000" & "1111001"; -- [G] [A] [t] [E] 
-					mode_vout <= '0' & x"2"; 
+					o_7seg_dat <= "0111101" & "1110111" & "1110000" & "1111001"; -- [G] [A] [t] [E] 
+					o_r2r_vout <= '0' & x"2"; 
 
 				-- m2: ROOM -> 0V6
 				when (m2) => 				
-					mode_display <= "1010000" & "1011100" & "1011100" & "0101011"; -- [r] [o] [o] [M] 
-					mode_vout <= '0' & x"5";
+					o_7seg_dat <= "1010000" & "1011100" & "1011100" & "0101011"; -- [r] [o] [o] [M] 
+					o_r2r_vout <= '0' & x"5";
 					
 				-- m3: HALL -> 0V9
 				when (m3) => 				
-					mode_display <= "1110110" & "1110111" & "0111000" & "0111000"; -- [H] [A] [L] [L] 
-					mode_vout <= '0' & x"8";
+					o_7seg_dat <= "1110110" & "1110111" & "0111000" & "0111000"; -- [H] [A] [L] [L] 
+					o_r2r_vout <= '0' & x"8";
 
 				-- m4: PLTE -> 1V2
 				when (m4) => 				
-					mode_display <= "1110011" & "0111000" & "1110111" & "1110000"; -- [P] [L] [A] [t] 
-					mode_vout <= '0' & x"B";
+					o_7seg_dat <= "1110011" & "0111000" & "1110111" & "1110000"; -- [P] [L] [A] [t] 
+					o_r2r_vout <= '0' & x"B";
 
 				-- m5: SPRG -> 1V5	
 				when (m5) => 				
-					mode_display <= "1101101" & "1110011" & "1010000" & "0111101"; -- [S] [P] [r] [G] 
-					mode_vout <= '0' & x"F";
+					o_7seg_dat <= "1101101" & "1110011" & "1010000" & "0111101"; -- [S] [P] [r] [G] 
+					o_r2r_vout <= '0' & x"F";
 
 				-- default state
 				when others =>
-					mode_display <= "0101011" & "0111111" & "1011110" & "0000000"; -- [M] [O] [d] [ ] 
-					mode_vout <= '0' & x"0"; 					
+					o_7seg_dat <= "0101011" & "0111111" & "1011110" & "0000000"; -- [M] [O] [d] [ ] 
+					o_r2r_vout <= '0' & x"0"; 					
 	
 				end case; 
 		end if; 
--- cpld/hdl/rtl/rv5_top.vhd
+++ cpld/hdl/rtl/rv5_top.vhd
@@ -7,30 +7,30 @@ generic (
 );
 port (
 	-- clocks
-	clk_m       : in  STD_LOGIC; -- master clock
+	i_clk       : in  STD_LOGIC; -- master clock
 	-- enable i/f
 --	ena_sw      : in  STD_LOGIC; -- fx enable slide switch
 --	ena_cv      : in  STD_LOGIC; -- fx enable CV input
 --	ena_cv_dv   : in  STD_LOGIC; -- fx enable CV detected
 --	ena_out     : out STD_LOGIC; -- fx enable to pedal
 	-- mode i/f
-	mode_pb     : in  STD_LOGIC_VECTOR (5 downto 0); -- mode pushbutton detected
-	mode_r2r    : out STD_LOGIC_VECTOR (R2R_SIZE-1 downto 0); -- r2r output bias
-	mode_led_segment_data : out STD_LOGIC_VECTOR (6 downto 0);  -- mode LED pulldown
-	mode_led_active_digit : out STD_LOGIC_VECTOR (3 downto 0)  -- LED digit pullup
+	i_pushbttn : in  STD_LOGIC_VECTOR (5 downto 0); -- mode pushbutton detected
+	o_r2r_vout : out STD_LOGIC_VECTOR (R2R_SIZE-1 downto 0); -- r2r output bias
+	o_7seg_dat : out STD_LOGIC_VECTOR (6 downto 0);  -- mode LED pulldown
+	o_7seg_sel : out STD_LOGIC_VECTOR (3 downto 0)  -- LED digit pullup
 ); end rv5_top;
 
 architecture rtl of rv5_top is
 
 	constant c_num_seg : natural := 7; -- segments per digit
 	constant c_num_dig : natural := 4; -- number of digits
-	signal mode_display : std_logic_vector(c_num_seg*c_num_dig-1 downto 0); -- vector for 4x 7seg digits 
+	signal w_7seg_dat : std_logic_vector(c_num_seg*c_num_dig-1 downto 0); -- vector for 4x 7seg digits 
 
 begin
 
 --	INST_FX_TOG : entity work.fx_toggle(rtl)
 --		port map (
---			clk    => clk_m,
+--			clk    => i_clk,
 --			cv_dv  => ena_cv_dv,
 --			cv     => ena_cv,
 --			sw     => ena_sw,
@@ -44,11 +44,15 @@ begin
     	g_num_dig => c_num_dig
 	)
 	port map (
-		clk          => clk_m,
-		mode_pb      => mode_pb, -- active low
-		mode_display => mode_display, -- active low
-		mode_vout	 => mode_r2r
-	); 
+		i_clk       => i_clk,
+		--! active lo input from the PCB
+		i_pushbttn  => i_pushbttn,
+		--! set 7-segement data vector based on the pushbutton
+		o_7seg_dat  => w_7seg_dat,  
+		--! active hi output to the PCD
+		o_r2r_vout	=> o_r2r_vout
+	); 	  
+	  
 
 
 	INST_7SEG_DRVR : entity work.sev_seg_drvr(rtl)
@@ -59,16 +63,16 @@ begin
     g_num_dig => 4
 	)
 	port map (
-    i_clk			    => clk_m, -- : in  std_logic;
+    i_clk			    => i_clk, -- : in  std_logic;
     --! input segment data
-    i_dig_1 		  => mode_display( c_num_seg*(c_num_dig-0)-1 downto c_num_seg*(c_num_dig-1) ), -- : in (6:0); 7seg vector for left digit
-    i_dig_2 		  => mode_display( c_num_seg*(c_num_dig-1)-1 downto c_num_seg*(c_num_dig-2) ), -- : in (6:0); 7seg vector for ...
-    i_dig_3 		  => mode_display( c_num_seg*(c_num_dig-2)-1 downto c_num_seg*(c_num_dig-3) ), -- : in (6:0); 7seg vector for ...
-    i_dig_4 		  => mode_display( c_num_seg*(c_num_dig-3)-1 downto c_num_seg*(c_num_dig-4) ), -- : in (6:0); 7seg vector for right digit
+    i_dig_1 		  => w_7seg_dat( c_num_seg*(c_num_dig-0)-1 downto c_num_seg*(c_num_dig-1) ), -- : in (6:0); 7seg vector for left digit
+    i_dig_2 		  => w_7seg_dat( c_num_seg*(c_num_dig-1)-1 downto c_num_seg*(c_num_dig-2) ), -- : in (6:0); 7seg vector for ...
+    i_dig_3 		  => w_7seg_dat( c_num_seg*(c_num_dig-2)-1 downto c_num_seg*(c_num_dig-3) ), -- : in (6:0); 7seg vector for ...
+    i_dig_4 		  => w_7seg_dat( c_num_seg*(c_num_dig-3)-1 downto c_num_seg*(c_num_dig-4) ), -- : in (6:0); 7seg vector for right digit
     --! digit select driver (pullup)
-    o_drive_dig		=> mode_led_active_digit, -- : out std_logic_vector(g_num_digits-1 downto 0);
+    o_drive_dig		=> o_7seg_sel, -- : out std_logic_vector(g_num_digits-1 downto 0);
     --! segment select driver (pulldown)
-    o_drive_seg 	=> mode_led_segment_data -- : out std_logic_vector(6 downto 0);	
+    o_drive_seg 	=> o_7seg_dat -- : out std_logic_vector(6 downto 0);	
 	);
 
 end rtl;
